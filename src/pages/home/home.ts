fd8a635323299dd75178f5434a2ebc713c51cfd9
kavitha <kavitha.madhavaraj@gmail.com>
2017-11-22 18:55:37 +0530

    Map page added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component,ViewChild,ElementRef } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+declare var google;
+/**
+ * Generated class for the MapPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  @ViewChild("map") mapRef: ElementRef;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+    this.showMap()
+  }
+
+  showMap(){
+    let pin = new google.maps.LatLng(51.507351,-0.127758);
+
+    let options = {
+      center: pin,
+      zoom:10
+    }
+
+    let map = new google.maps.Map(this.mapRef.nativeElement,options);
+  }
 }
