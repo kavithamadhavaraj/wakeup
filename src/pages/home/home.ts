3c021f99e458b35e423f77472c805e4f606e97cb
kavitha <kavitha.madhavaraj@gmail.com>
2017-11-25 12:43:26 +0530

    Geolocation and Autocompete included

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component,ViewChild,ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-declare var google;
+declare let google: any;
 /**
  * Generated class for the MapPage page.
  *
@@ -14,23 +14,67 @@ declare var google;
   templateUrl: 'home.html',
 })
 export class HomePage {
-  @ViewChild("map") mapRef: ElementRef;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  
   }
+  destination: String;
+  locationErrorInfo: string = null;
+
+  findUserLocation<Promise>(){
+    return new Promise<any>((resolve,reject) => {
+      if(!!navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {             
+          let geolocation:any = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          resolve(geolocation);
+        },function(error){  
+            reject(error.message);
+        });      
+      } else {
+        reject("GPS not supported");
+      }
+    })
+  }
+
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
-    this.showMap()
+      this.findUserLocation().then((pin)=>{
+        this.showMap(pin);
+      }).catch((error)=>{
+        this.locationErrorInfo = error;
+      });  
   }
 
-  showMap(){
-    let pin = new google.maps.LatLng(51.507351,-0.127758);
+  suggest(event_obj){
+    let selectedPin = null;
+    let autocomplete = new google.maps.places.Autocomplete(event_obj.target, {types: ['geocode']});
+    google.maps.event.addListener(autocomplete, 'place_changed', () => {      
+      let place = autocomplete.getPlace();
+      if(place.geometry){
+        let latitude = place.geometry.location.lat();
+        let longitude = place.geometry.location.lng();
+        selectedPin = new google.maps.LatLng(latitude, longitude);
+        this.showMap(selectedPin);        
+      }     
+    });
+    if((event_obj.keyCode == 13) && (selectedPin == null)){
+      //TODO: TOAST CONTROLLER
+      console.log("Oops.. Couldn't find that location. Try another landmark !")
+    }
+      
+  }
 
+  showMap(pin){
     let options = {
       center: pin,
-      zoom:10
+      zoom:15,
+      gestureHandling : "cooperative",
+      streetViewControl: false
     }
-
-    let map = new google.maps.Map(this.mapRef.nativeElement,options);
+    let map = new google.maps.Map(document.getElementById("map"), options);
+    let marker = new google.maps.Marker({
+      position: pin,
+      map: map
+    });         
+    map.setCenter(pin);
   }
 }
